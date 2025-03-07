38732b32ab30fcbd02fbc2765fbb2936aaec5ed4
jessevanatta <jessevanatta@gmail.com>
2025-03-07 08:26:43 -0800

    drawing player to screen

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 import pygame
 from constants import *
+from circleshape import CircleShape
+from player import Player
 
 def main():
+    # Initialize
     pygame.init()
     time = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    #Create player
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    # Game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
         dt = time.tick(60) / 1000
     print("Starting Asteroids!")
